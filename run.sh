cfb9573d6fff3a211a206f9d3150de0f6a5a5dab
Root <interactivetoyscompany@gmail.com>
2021-08-18 17:24:03 -0500

    Update file

---
+++ run.sh
@@ -0,0 +1,6 @@
+cd /home/pi/Documents/App/App_Prologer/dist/Main8
+./Main8
+#source /home/pi/Documents/env/bin/activate
+#python3 ~/Documents/PLC/Programming_Mode/Main8.py
+#https://yotube.com/watch?v=cZa1oCSdbUk
+#https://github.com/pyinstaller/issues/3810
---
+++ update.sh
@@ -0,0 +1,5 @@
+echo $1
+unzip -o /home/pi/Documents/App/App_Prologer/$1 -d /home/pi/Documents/App/App_Prologer/
+rm -rf   /home/pi/Documents/App/App_Prologer/$1 
+#sudo reboot
+
